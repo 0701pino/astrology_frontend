709c67a497e96bb34817d436c3ea6f88f3dc9a75
pino <0701pino@kumonekodo.com>
2023-05-24 12:19:10 +0000

    タイトルにトップへのリンクを設定

--- my-astrology-site/src/components/Header.tsx
+++ my-astrology-site/src/components/Header.tsx
@@ -1,10 +1,10 @@
-
+import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <header className="bg-gray-900 text-white">
       <div className="container mx-auto py-4">
         <h1 className="text-3xl font-bold text-center">
-          星占い
+          <Link to="/">星占い</Link>
         </h1>
       </div>
     </header>
